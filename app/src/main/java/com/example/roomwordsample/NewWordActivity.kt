4e6b2ff3ca836a0f618b6ad009f22b5fdf1e1519
Gyula Juhász <gyu999@gmail.com>
2019-05-19 20:22:34 +0200

    Small refinements.

--- app/src/main/java/com/example/roomwordsample/NewWordActivity.kt
+++ app/src/main/java/com/example/roomwordsample/NewWordActivity.kt
@@ -4,26 +4,20 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
-import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_new_word.*
 
 class NewWordActivity : AppCompatActivity() {
-
-    private lateinit var editWordView: EditText
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_word)
-        editWordView = findViewById(R.id.edit_word)
-
-        val button = findViewById<Button>(R.id.button_save)
-        button.setOnClickListener {
+        button_save.setOnClickListener {
             val replyIntent = Intent()
-            if (TextUtils.isEmpty(editWordView.text)) {
+            if (TextUtils.isEmpty(edit_word.text)) {
                 setResult(Activity.RESULT_CANCELED, replyIntent)
             } else {
-                val word = editWordView.text.toString()
+                val word = edit_word.text.toString()
                 replyIntent.putExtra(EXTRA_REPLY, word)
                 setResult(Activity.RESULT_OK, replyIntent)
             }
